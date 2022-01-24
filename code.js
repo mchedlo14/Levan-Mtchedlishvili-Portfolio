2f78c368ad883beee54306479befa90f67bd5824
unknown <mchedlo14>
2022-01-24 15:32:28 +0400

    portfolio added about page ;

--- code.js
+++ code.js
@@ -1,3 +1,16 @@
 AOS.init({
     duration :1000
 });
+
+function effects(){
+    $('#li-contact').click(function(){
+        $('.slidedown-div').slideDown('slow')
+        $('.slidedown-div').css('display','flex')
+    })
+
+    $('.close-icon').click(function(){
+        $('.slidedown-div').slideUp('slow')
+    })
+}
+
+effects()
